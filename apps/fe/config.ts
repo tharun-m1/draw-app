dcba9e2f1d4b8bbed14c4cc24857c7f4855c458d
Tharun M <maddelatharun2@gmail.com>
2025-03-07 09:08:20 +0530

    updated api

--- apps/fe/config.ts
+++ apps/fe/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:4000/api/v1"
-export const WS_URL = "ws://localhost:8080"
+export const BACKEND_URL = "https://sketch.pheonixclub.com/api/v1"
+export const WS_URL = "wss://sketchchat.pheonixclub.com"
